1d675cf7dc5ecb017b975ea9dc88ecfade159ca3
lxxxvi <lxxxvi@users.noreply.github.com>
2020-04-03 17:39:07 +0200

    basic model tests for Release

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,2 +1,4 @@
 class Release < ApplicationRecord
+  validates :catalog_number, :artist, :name, :released_on, :playlist, presence: true
+  validates :catalog_number, uniqueness: true
 end
--- test/models/release_test.rb
+++ test/models/release_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ReleaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'mandatory fields' do
+    release = Release.new
+    release.validate
+
+    release.errors.messages.tap do |messages|
+      assert_equal ["can't be blank"], messages[:catalog_number]
+      assert_equal ["can't be blank"], messages[:artist]
+      assert_equal ["can't be blank"], messages[:name]
+      assert_equal ["can't be blank"], messages[:released_on]
+      assert_equal ["can't be blank"], messages[:playlist]
+    end
+  end
+
+  test 'uniqueness' do
+    release = Release.new(catalog_number: 'HSH001')
+    release.validate
+
+    assert_equal ['has already been taken'], release.errors.messages[:catalog_number]
+  end
 end
