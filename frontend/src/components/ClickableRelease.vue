185ecfb94ef0b57c34d7f266e6ef77d37c33c8b8
lxxxvi <lxxxvi@users.noreply.github.com>
2020-08-23 13:14:38 +0200

    add tailwind, introduce ClickableRelease

---
+++ frontend/src/components/ClickableRelease.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="$emit('show-release-id-event', releaseId)" >
+      {{ releaseId }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['releaseId']
+}
+</script>
+
+<style scoped>
+button {
+  @apply p-5 bg-gray-300;
+}
+</style>
--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -1,26 +1,28 @@
 <template>
   <div id="#releases" class="h-screen bg-gradient-to-b from-white to-gray-200">
+    <div class="mx-auto">
+      <h1 class="text-6xl font-bold break-all tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-teal-600 uppercase leading-none mt-10 mb-5 inline-block m-auto">
+            Home<br />
+            Street<br />
+            Home
+      </h1>
+      <div>
+        <span class="inline-block uppercase bg-clip-text text-transparent text-2xl font-bold bg-gradient-to-r from-indigo-700 to-teal-700 tracking-wide">records</span>
+      </div>
 
-    <h1 class="text-6xl font-bold break-all tracking-wide bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-teal-600 uppercase leading-none mt-10 mb-5 inline-block m-auto">
-          Home<br />
-          Street<br />
-          Home
-    </h1>
-    <span class="inline-block uppercase bg-clip-text text-transparent text-2xl font-bold bg-gradient-to-r from-indigo-700 to-teal-700 tracking-wide">records</span>
-    <button @click="$emit('show-release-id-event', 1)">1</button>
-    <button @click="$emit('show-release-id-event', 2)">2</button>
-    <button @click="$emit('show-release-id-event', 3)">3</button>
-    <button @click="$emit('show-release-id-event', 4)">4</button>
+      <ClickableRelease :release-id="1" v-on="$listeners" />
+      <ClickableRelease :release-id="2" v-on="$listeners" />
+      <ClickableRelease :release-id="3" v-on="$listeners" />
+      <ClickableRelease :release-id="4" v-on="$listeners" />
+      <ClickableRelease :release-id="5" v-on="$listeners" />
+    </div>
   </div>
 </template>
 
 <script>
+import ClickableRelease from './ClickableRelease.vue'
+
 export default {
+  components: { ClickableRelease }
 }
 </script>
-
-<style scoped>
-button {
-  display: block;
-}
-</style>
