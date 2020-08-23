3aaacd7afbac5f9099edb3f00acf7f3bb6d0c9c9
lxxxvi <lxxxvi@users.noreply.github.com>
2020-08-23 10:53:36 +0200

    cleanup javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("@rails/activestorage").start()
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,9 +12,7 @@ require("@rails/activestorage").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "controllers"
-
-require("css/application.scss")
+require("css/application.css")
 
 document.addEventListener("DOMContentLoaded", function(){
   Array.from(document.querySelectorAll('[data-url]')).forEach((element) => {
@@ -25,5 +21,3 @@ document.addEventListener("DOMContentLoaded", function(){
     });
   });
 });
-
-
