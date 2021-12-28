7527de175a14d4880e360267887ae815424f4639
mario <github@lxxxvi.ch>
2021-12-28 15:12:01 +0100

    remove obsolete stuff

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,9 @@
 // Entry point for the build script in your package.json
+
+document.addEventListener("DOMContentLoaded", function(){
+  Array.from(document.querySelectorAll('[data-url]')).forEach((element) => {
+    element.addEventListener('click', (event) => {
+      window.location.href = element.dataset.url;
+    });
+  });
+});
